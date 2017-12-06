c481e6c8a00a678c1231124f7edde71bea979890
fraasssee <frans.holmgren@burtcorp.com>
2017-12-06 19:41:35 +0100

    Add Task 1 for Day 6

---
+++ day6/task1.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var Sugar = require('sugar');
+Sugar.extend();
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+  var array = data.split(' ').compact(true).map(i => parseInt(i));
+  var combinations = {}
+  var counter = 0
+  while (!combinations[array.join('')]) {
+    combinations[array.join('')] = 1;
+    var indexLargest = 0;
+    array.forEach((item, i) => item > array[indexLargest] ? indexLargest = i : null);
+    var numbers = array[indexLargest]
+    array[indexLargest] = 0;
+    for (var i = 1; i <= numbers; i++) {
+      array[(indexLargest + i) % array.length]++;
+    }
+    counter++;
+  }
+  console.log(counter)
+})
